72ce6fa05412b04fae5bc8d2ed70bff50c65975a
Daniel <99733108+dannythedev@users.noreply.github.com>
2022-02-17 13:20:04 +0200

    Update ex001.py

--- ex001.py
+++ ex001.py
@@ -1,9 +1,6 @@
 # Project Euler 1 - Multiples of 3 or 5. Solved in 0.00020090000000000385 seconds runtime.
 # Exercise: https://projecteuler.net/problem=1
 
-import timeit
-start = timeit.default_timer()
-
 def ex1():
     sum=0
     for x in range(1,1000):
@@ -11,6 +8,3 @@ def ex1():
             sum+=x
     return sum
 print(ex1())
-
-stop = timeit.default_timer()
-print('Time: ', stop - start,"seconds.")
