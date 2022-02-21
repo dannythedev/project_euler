b5cd6b49f1f18deb8a4df189f18c59247e58d321
dannythedev <dannythedev@users.noreply.github.com>
2022-02-21 22:41:35 +0200

    Update ex021.py

--- ex021.py
+++ ex021.py
@@ -1,13 +1,13 @@
 # Project Euler 21, Amicable numbers. Solved in 0.298 seconds runtime.
 # Exercise: https://projecteuler.net/problem=21
 
-def sum_of_divisors(n):
+def sum_of_proper_divisors(n):
     lim = int(n ** 0.5) + 1
-    return sum([n,1]+[f for d in range(2,lim) for f in (d,n/d) if (n%d==0)])
+    return sum([1]+[f for d in range(2,lim) for f in (d,n/d) if (n%d==0)])
 
 def has_amicable_pair(n):
-    d1 = sum_of_divisors(n)-n #Not including n itself.
-    d2 = sum_of_divisors(d1)-d1 #Not including d1 itself.
+    d1 = sum_of_proper_divisors(n)
+    d2 = sum_of_proper_divisors(d1)
     return d2==n and n!=d1
 
 def ex21():
