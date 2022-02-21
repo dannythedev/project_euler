0506c84f4086a2c0f94896683a3a3b602eaab139
Daniel <99733108+dannythedev@users.noreply.github.com>
2022-02-21 21:01:31 +0200

    Create ex020.py

---
+++ ex020.py
@@ -0,0 +1,19 @@
+# Project Euler 20 - Factorial digit sum. Solved in 0.0002926 seconds runtime.
+# Exercise: https://projecteuler.net/problem=20
+
+def factorial(n):
+    if n<=2:
+        return n
+    return n*factorial(n-1)
+
+def sum_of_digits(n):
+    s = 0
+    while n>0:
+        s += n % 10
+        n//=10
+    return s
+
+def ex20():
+    return sum_of_digits(factorial(100))
+
+print(ex20())
