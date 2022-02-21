14580f92a0bb4bd8fc4454dd280ee612902a7d81
dannythedev <dannythedev@users.noreply.github.com>
2022-02-21 23:04:26 +0200

    Update ex021.py

--- ex021.py
+++ ex021.py
@@ -3,7 +3,7 @@
 
 def sum_of_proper_divisors(n):
     lim = int(n ** 0.5) + 1
-    return sum([1]+[f for d in range(2,lim) for f in (d,n/d) if (n%d==0)])
+    return sum(set([1]+[f for d in range(2,lim) for f in (d,n/d) if (n%d==0)]))
 
 def has_amicable_pair(n):
     d1 = sum_of_proper_divisors(n)
