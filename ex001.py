cfbb4fd59ef59647c371309b77f3eb64d619aafa
Daniel <99733108+dannythedev@users.noreply.github.com>
2022-07-15 13:31:39 +0300

    Update ex001.py

--- ex001.py
+++ ex001.py
@@ -1,7 +1,15 @@
-# Project Euler 1 - Multiples of 3 or 5. Solved in 0.00014 seconds runtime.
+# Project Euler 1 - Multiples of 3 or 5. Solved in 0.0001 seconds runtime.
 # Exercise: https://projecteuler.net/problem=1
 
 def ex1():
-    return sum([x for x in range(1,10**7) if x%3==0 or x%5==0])
+    sum, x = 0, 1
+    a3, a5 = 0, 0
+    while a3 < 1000:
+        if a5 < 1000 and not a5 % 3 == 0:
+            sum += a5
+        sum += a3
+        a3, a5 = x * 3, x * 5
+        x += 1
+    return sum
 
 print(ex1())
